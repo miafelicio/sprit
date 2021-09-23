702ccbc93706ebfd059b21b89ca8c3fd014de36f
miafelicio <miriafelicio7@gmail.com>
2021-09-23 11:19:56 -0300

    dml quase pronto

--- 1.4-exercicio-optus/Scripts/DML.sql
+++ 1.4-exercicio-optus/Scripts/DML.sql
@@ -6,7 +6,32 @@ VALUES
 ('justin baby'),
 ('rodigo olivo'),
 ('má dona'),
-('carlin robertin')
+('xuxa')
 GO
 
-INSERT INTO ALBUM(tituloAlbum, dataLancamento, ativo, 
+INSERT INTO ALBUM(tituloAlbum, dataLancamento, ativo, idArtista)
+VALUES
+('pra mexer o esqueleto', '21/02/2020', 'sim', 4),
+('mexer e mexer', '12/05/2008', 'nao', 1),
+('so os bois', '30/10/21', 'sim', 2),
+('funk pra baixinhos', '02/04/2010', 'nao', 3)
+GO
+
+INSERT INTO ESTILO(nomeEstilo)
+VALUES
+('rock na veia'),
+('funk'),
+('sertanejo')
+GO
+
+INSERT INTO ALBUM_ESTILO(idAlbum, idEstilo)
+VALUES
+(1, 2),
+(2, 1),
+(3, 3),
+(4, 2)
+GO
+
+INSERT INTO USUARIO(nomeUsuario, emailUsuario, senhaUsuario, permissao)
+VALUES
+('astolfo', 'astolfinhogameplay12@gmail.com', 'mufshnfhydsnf', 'não'),
